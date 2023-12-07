dc81e19968f256a1d76ff1024416dbf3a372cf5f
monyksam27 <145047900+monyksam27@users.noreply.github.com>
2023-12-07 04:29:03 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,17 +1,22 @@
 import time
 
+def print_slow(text):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(0.05)
+    print()
 
 # timing
 def introduction():
     # prompt user to write a name
-    Name_of_astronaut = str(input("What is your name Brave astronaut "))
-    print(
+    Name_of_astronaut = str(input("What is your name Brave astronaut? (Write your name): "))
+    print_slow(
         f"Welcome, astronaut {Name_of_astronaut}! You are stationed on the research outpost Perseus, orbiting Nexus-9."
     )
-    print("Your mission is to investigate anomalies and manifestations around Nexus-9.")
-    print("Be cautious, as the planet seems to respond to thoughts and memories.")
+    print_slow("Your mission is to investigate anomalies and manifestations around Nexus-9.")
+    print_slow("Be cautious, as the planet seems to respond to thoughts and memories.")
     time.sleep(2)
-    print("\nYou wake up in your quarters. It's eerily silent.")
+    print_slow("\nYou wake up in your quarters. It's eerily silent.")
 
 
 def explore_station():
@@ -42,22 +47,47 @@ def confrontation():
     print("1. Try to confront the illusions.")
     print("2. Attempt to ground yourself and focus on reality.")
 
+class InvalidInputError(Exception):
+    pass
+
+class InvalidInputError(Exception):
+    pass
+
+def valid_value(prompt):
+    while True:
+        try:
+            value = str(input(prompt)).lower()
+            if value not in ['yes', 'no']:
+                raise InvalidInputError("Please enter 'yes' or 'no'.")
+            return value
+        except InvalidInputError as e:
+            print(e)
+
+def choice(prompt):
+    while True:
+        try:
+            value = str(input(prompt)).lower()
+            if value not in ['1', '2']:
+                raise InvalidInputError("Please enter '1' or '2'.")
+            return value
+        except InvalidInputError as e:
+            print(e)
+
 
 def main_game():
     introduction()
 
     # 1 choice
-    explore_station_choice = input(
-        "\nDo you want to explore the station? (yes/no): "
-    ).lower()
+    explore_station_choice = valid_value("\nDo you want to explore the station? (yes/no): ").lower()
+    
     if explore_station_choice == "yes":
         explore_station()
-        station_option = input("\nEnter your choice (1/2): ")
+        station_option = choice("\nEnter your choice (1/2): ")
 
         # Second choice //the first option
         if station_option == "1":
             investigate_light()
-            light_option = input("\nEnter your choice (1/2): ")
+            light_option = choice("\nEnter your choice (1/2): ")
 
             # Final choice based on previous options
             if light_option == "1":
@@ -72,10 +102,11 @@ def main_game():
                 "\nYou reach the observation deck and witness breathtaking cosmic phenomena."
             )
             # Continue game based on the chosen path
-    else:
+    elif explore_station_choice == "no":
         print("\nYou decide to stay in your quarters.")
         # Continue game based on the chosen path
-
+    else:
+        print("Invalid input. Try again")
 
 if __name__ == "__main__":
     main_game()
